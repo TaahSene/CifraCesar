9419120fa993d0b9e303272586112aca19493890
Taynara Sene <tay.sene@gmail.com>
2017-03-07 17:58:02 -0300

    teste app vue

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-new Vue({
+// var app5 = new Vue({
+//   el: '#app-5',
+//   data: {
+//     message: 'Hello Vue.js!'
+//   },
+//   methods: {
+//     reverseMessage: function () {
+//       this.message = this.message.split('').reverse().join('')
+//     }
+//   }
+// })
 
-});
+
+var app = new Vue({
+  el: '#app',
+  methods: {
+    encryptMessage: function () {
+			console.log(this);
+    }
+  }
+})
