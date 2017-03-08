2ae6d9c049115a49d463344c6f06c3210e54ef2e
Taynara Sene <tay.sene@gmail.com>
2017-03-08 02:18:36 -0300

    encrypt components

--- js/app.js
+++ js/app.js
@@ -1,21 +1,3 @@
-// var app5 = new Vue({
-//   el: '#app-5',
-//   data: {
-//     message: 'Hello Vue.js!'
-//   },
-//   methods: {
-//     reverseMessage: function () {
-//       this.message = this.message.split('').reverse().join('')
-//     }
-//   }
-// })
-
-
-var app = new Vue({
-  el: '#app',
-  methods: {
-    encryptMessage: function () {
-			console.log(this);
-    }
-  }
+new Vue({
+  el: '#vue'
 })
