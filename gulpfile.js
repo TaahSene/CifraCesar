2ddff6c8fb5686d644da9972d24521096393d363
Taynara Sene <tay.sene@gmail.com>
2017-03-08 10:16:19 -0300

    gulp task fonts

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,12 @@ var JSFiles = [
 var CSSFiles =[
 	'node_modules/semantic-ui/dist/semantic.min.css',
 	'css/app.css'];
+var FONTFiles =[
+	'node_modules/semantic-ui/dist/themes/default/assets/fonts/*.*'];
 
 var destinationJs = 'js';
 var destinationCss = 'css'; 
+var destinationFont = 'css/themes/default/assets/fonts'
 
 gulp.task('copyJs', function () {
     gulp.src(JSFiles)
@@ -20,4 +23,9 @@ gulp.task('copyJs', function () {
 gulp.task('copyCss', function () {
     gulp.src(CSSFiles)
         .pipe(gulp.dest(destinationCss));
+});
+
+gulp.task('copyFonts', function () {
+    gulp.src(FONTFiles)
+        .pipe(gulp.dest(destinationFont,{"mode": "0777"}));
 });
